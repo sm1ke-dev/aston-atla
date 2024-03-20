560f8401611067337685b0a754f3de2201f431b0
sm1ke <smike143@yandex.ru>
2024-03-21 00:44:54 +0500

    feat: создать страницу Main

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,22 +1,15 @@
 import React from "react";
+import styles from "./Main.module.scss";
+import CardsList from "../../components/CardsList/CardsList";
+import SearchForm from "../../components/SearchForm/SearchForm";
 import logo from "../../logo.svg";
 
 const Main: React.FC = () => {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <main className={styles.main}>
+      <SearchForm />
+      <CardsList />
+    </main>
   );
 };
 
