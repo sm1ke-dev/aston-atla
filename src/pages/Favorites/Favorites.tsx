ca39da4951006cdf732906fe1c71643db33f7c1a
sm1ke <smike143@yandex.ru>
2024-03-21 14:04:40 +0500

    feat: создать страницу избранное

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import styles from "./Favorites.module.scss";
+import CardsList from "../../components/CardsList/CardsList";
 
 const Favorites = () => {
   return (
-    <div>
-      <h1>Избранное</h1>
-    </div>
+    <main className={styles.favs}>
+      <h2 className={styles.favs__title}>Избранное</h2>
+      {/* <h2 className={styles.favs__title}>Вы еще ничего не добавили в избранное</h2> */}
+      <CardsList />
+    </main>
   );
 };
 
