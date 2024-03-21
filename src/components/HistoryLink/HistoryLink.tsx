eb060a5d865019db6c3b049faf83f5fd01d681d6
sm1ke <smike143@yandex.ru>
2024-03-21 14:32:57 +0500

    feat: создать страницу истории поиска

---
+++ src/components/HistoryLink/HistoryLink.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./HistoryLink.module.scss";
+import { Link } from "react-router-dom";
+
+const HistoryLink = () => {
+  const [isHovered, setIsHovered] = React.useState(false);
+
+  return (
+    <li
+      className={styles.item}
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
+    >
+      <Link to="/search" className={styles.item__link}>
+        Aang
+      </Link>
+      {isHovered && (
+        <button className={styles.item__button}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="100"
+            height="100"
+            viewBox="0,0,256,256"
+            className={styles.item__icon}
+          >
+            <g
+              fill="#ffffff"
+              fillRule="nonzero"
+              stroke="none"
+              strokeWidth="1"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeMiterlimit="10"
+              strokeDasharray=""
+              strokeDashoffset="0"
+              fontFamily="none"
+              fontWeight="none"
+              fontSize="none"
+              textAnchor="none"
+            >
+              <g transform="scale(10.66667,10.66667)">
+                <path d="M4.70703,3.29297l-1.41406,1.41406l7.29297,7.29297l-7.29297,7.29297l1.41406,1.41406l7.29297,-7.29297l7.29297,7.29297l1.41406,-1.41406l-7.29297,-7.29297l7.29297,-7.29297l-1.41406,-1.41406l-7.29297,7.29297z"></path>
+              </g>
+            </g>
+          </svg>
+        </button>
+      )}
+    </li>
+  );
+};
+
+export default HistoryLink;
--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import HistoryLink from "../../components/HistoryLink/HistoryLink";
+import styles from "./History.module.scss";
 
 const History = () => {
   return (
-    <div>
-      <h1>История поиска</h1>
-    </div>
+    <main className={styles.history}>
+      <h2 className={styles.history__title}>История поиска:</h2>
+      <ul className={styles.history__list}>
+        <HistoryLink />
+        <HistoryLink />
+        <HistoryLink />
+        <HistoryLink />
+        <HistoryLink />
+      </ul>
+    </main>
   );
 };
 
