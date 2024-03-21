8ed536b752836fe32735f198c8dc2fef567c515a
sm1ke <smike143@yandex.ru>
2024-03-22 00:14:53 +0500

    feat: расположить данные с сервера на главную страницу

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,17 +2,25 @@ import React from "react";
 import styles from "./Main.module.scss";
 import SearchForm from "../../components/SearchForm/SearchForm";
 import CardItem from "../../components/CardItem/CardItem";
+import { useGetAllCharactersQuery } from "../../redux/atlaApi";
 
 const Main: React.FC = () => {
+  const { data: cards, isLoading, isError, error } = useGetAllCharactersQuery();
+
+  if (isLoading) {
+    return <h2 className={styles.main__info}>Загрузка...</h2>;
+  }
+
+  if (isError) {
+    console.log(error);
+    return <h2 className={styles.main__info}>Произошла ошибка</h2>;
+  }
+
   return (
     <main className={styles.main}>
       <SearchForm />
       <ul className={styles.main__list}>
-        <CardItem />
-        <CardItem />
-        <CardItem />
-        <CardItem />
-        <CardItem />
+        {cards?.map((card) => <CardItem key={card._id} {...card} />)}
       </ul>
     </main>
   );
