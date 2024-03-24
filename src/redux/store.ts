097bda00563b156632ed07030c6bd454d0ad9fe5
sm1ke <smike143@yandex.ru>
2024-03-25 02:59:22 +0500

    feat: подключить слайс к store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -10,3 +10,6 @@ export const store = configureStore({
   middleware: (getDefaultMiddlware) =>
     getDefaultMiddlware().concat(atlaApi.middleware),
 });
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
