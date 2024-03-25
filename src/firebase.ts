0dc9b108beb7727eb9914b981002860d0decb50e
sm1ke <smike143@yandex.ru>
2024-03-26 01:37:43 +0500

    feat: установить базу данных в проект

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDtDxolro8LZQipZvykM8gfKFQdE9FF2t4",
@@ -8,8 +9,11 @@ const firebaseConfig = {
   storageBucket: "aston-atla.appspot.com",
   messagingSenderId: "1015331362592",
   appId: "1:1015331362592:web:dfb482f39d5eea3ec8a945",
+  databaseURL:
+    "https://aston-atla-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getDatabase(app);
